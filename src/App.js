8e2a4cd203320f40a9cd08608da9a9e1fc140d26
Noelia Donato <nsdonatovignais@teco.com.ar>
2022-07-18 20:40:15 -0300

    feat(notifications): add notifications to app

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import { SinglePostPage } from './features/posts/SinglePostPage'
 import { EditPostForm } from './features/posts/EditPostForm'
 import { UsersList } from './features/users/UsersList'
 import { UserPage } from './features/users/UserPage'
+import { NotificationsList } from './features/notifications/NotificationsList'
 
 function App() {
   return (
@@ -34,6 +35,7 @@ function App() {
           <Route exact path="/editPost/:postId" component={EditPostForm} />
           <Route exact path="/users" component={UsersList} />
           <Route exact path="/users/:userId" component={UserPage} />
+          <Route exact path="/notifications" component={NotificationsList} />
           <Redirect to="/" />
         </Switch>
       </div>
---
+++ src/features/notifications/notificationsSlice.js
@@ -0,0 +1,45 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+
+import { client } from '../../api/client'
+
+export const fetchNotifications = createAsyncThunk(
+  'notifications/fetchNotifications',
+  async (_, { getState }) => {
+    const allNotifications = selectAllNotifications(getState())
+    const [latestNotification] = allNotifications
+    const latestTimestamp = latestNotification ? latestNotification.date : ''
+    const response = await client.get(
+      `/fakeApi/notifications?since=${latestTimestamp}`
+    )
+    return response.data
+  }
+)
+
+const notificationsSlice = createSlice({
+  name: 'notifications',
+  initialState: [],
+  reducers: {
+    allNotificationsRead(state, action) {
+      state.forEach((notification) => {
+        notification.read = true
+      })
+    },
+  },
+  extraReducers(builder) {
+    builder.addCase(fetchNotifications.fulfilled, (state, action) => {
+      state.push(...action.payload)
+      state.forEach((notification) => {
+        // Any notifications we've read are no longer new
+        notification.isNew = !notification.read
+      })
+      // Sort with newest first
+      state.sort((a, b) => b.date.localeCompare(a.date))
+    })
+  },
+})
+
+export const { allNotificationsRead } = notificationsSlice.actions
+
+export default notificationsSlice.reducer
+
+export const selectAllNotifications = (state) => state.notifications
