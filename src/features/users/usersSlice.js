71a1db5e10e8f9ab7fe4a8cd88ce3b86830fd50f
Noelia Donato <nsdonatovignais@teco.com.ar>
2022-07-18 19:46:51 -0300

    feat(pages): add pages to app

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -4,19 +4,24 @@ import { client } from '../../api/client'
 const initialState = []
 
 export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
-    const response = await client.get('/fakeApi/users')
-    return response.data
+  const response = await client.get('/fakeApi/users')
+  return response.data
 })
 
 const usersSlice = createSlice({
-    name: 'users',
-    initialState,
-    reducers: {},
-    extraReducers(builder) {
-        builder.addCase(fetchUsers.fulfilled, (state, action) => {
-            return action.payload
-        })
-    }
+  name: 'users',
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      return action.payload
+    })
+  },
 })
 
 export default usersSlice.reducer
+
+export const selectAllUsers = (state) => state.users
+
+export const selectUserById = (state, userId) =>
+  state.users.find((user) => user.id === userId)
