4b0e4747d1a63f4b7eeab1a7b64fe91e40eb8e51
Noelia Donato <noeliadonato@gmail.com>
2022-07-18 14:06:22 -0300

    part-5-async-logic#checking-thunk-results-in-components

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -13,6 +13,17 @@ export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
     return response.data
 })
 
+export const addNewPost = createAsyncThunk(
+    'posts/addNewPost',
+    // The payload creator receives the partial `{title, content, user}` object
+    async initialPost => {
+        // We send the initial data to the fake API server
+        const response = await client.post('/fakeApi/posts', initialPost)
+        // The response includes the complete post object, including unique ID
+        return response.data
+    }
+)
+
 const postsSlice = createSlice({
     name: 'posts',
     initialState,
@@ -69,6 +80,10 @@ const postsSlice = createSlice({
             .addCase(fetchPosts.rejected, (state, action) => {
                 state.status = 'failed'
                 state.error = action.error.message
+            })
+            .addCase(addNewPost.fulfilled, (state, action) => {
+                // We can directly add the new post object to our posts array
+                state.posts.push(action.payload)
             })
     }
 })
