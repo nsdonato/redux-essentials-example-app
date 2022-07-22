726f5079a6db0a59a5e2beb976301b3d6175fef1
Noelia Donato <nsdonatovignais@teco.com.ar>
2022-07-22 12:37:58 -0300

    part-6-performance-normalization#normalizing-data

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -7,59 +7,59 @@ import { ReactionButtons } from './ReactionButtons'
 import { selectAllPosts, fetchPosts } from './postsSlice'
 import { Spinner } from '../../components/Spinner'
 
-const PostExcerpt = ({ post }) => {
-    return (
-        <article className="post-excerpt" key={post.id}>
-            <h3>{post.title}</h3>
-            <div>
-                <PostAuthor userId={post.user} />
-                <TimeAgo timestamp={post.date} />
-            </div>
-            <p className="post-content">{post.content.substring(0, 100)}</p>
+let PostExcerpt = ({ post }) => {
+  return (
+    <article className="post-excerpt" key={post.id}>
+      <h3>{post.title}</h3>
+      <div>
+        <PostAuthor userId={post.user} />
+        <TimeAgo timestamp={post.date} />
+      </div>
+      <p className="post-content">{post.content.substring(0, 100)}</p>
 
-            <ReactionButtons post={post} />
-            <Link to={`/posts/${post.id}`} className="button muted-button">
-                View Post
-            </Link>
-        </article>
-    )
+      <ReactionButtons post={post} />
+      <Link to={`/posts/${post.id}`} className="button muted-button">
+        View Post
+      </Link>
+    </article>
+  )
 }
-
+PostExcerpt = React.memo(PostExcerpt)
 
 export const PostsList = () => {
-    const dispatch = useDispatch()
-    const posts = useSelector(selectAllPosts)
+  const dispatch = useDispatch()
+  const posts = useSelector(selectAllPosts)
 
-    const postStatus = useSelector(state => state.posts.status)
-    const error = useSelector(state => state.posts.error)
+  const postStatus = useSelector((state) => state.posts.status)
+  const error = useSelector((state) => state.posts.error)
 
-    useEffect(() => {
-        if (postStatus === 'idle') {
-            dispatch(fetchPosts())
-        }
-    }, [postStatus, dispatch])
-
-    let content
-
-    if (postStatus === 'loading') {
-        content = <Spinner text="Loading..." />
-    } else if (postStatus === 'succeeded') {
-        // Sort posts in reverse chronological order by datetime string
-        const orderedPosts = posts
-            .slice()
-            .sort((a, b) => b.date.localeCompare(a.date))
-
-        content = orderedPosts.map(post => (
-            <PostExcerpt key={post.id} post={post} />
-        ))
-    } else if (postStatus === 'failed') {
-        content = <div>{error}</div>
+  useEffect(() => {
+    if (postStatus === 'idle') {
+      dispatch(fetchPosts())
     }
+  }, [postStatus, dispatch])
 
-    return (
-        <section className="posts-list">
-            <h2>Posts</h2>
-            {content}
-        </section>
-    )
+  let content
+
+  if (postStatus === 'loading') {
+    content = <Spinner text="Loading..." />
+  } else if (postStatus === 'succeeded') {
+    // Sort posts in reverse chronological order by datetime string
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date))
+
+    content = orderedPosts.map((post) => (
+      <PostExcerpt key={post.id} post={post} />
+    ))
+  } else if (postStatus === 'failed') {
+    content = <div>{error}</div>
+  }
+
+  return (
+    <section className="posts-list">
+      <h2>Posts</h2>
+      {content}
+    </section>
+  )
 }
