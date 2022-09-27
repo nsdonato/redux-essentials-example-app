5652f7099b5f52ba2c72bf9b3fcb611751673203
Noelia Donato <nsdonatovignais@teco.com.ar>
2022-09-26 21:05:12 -0300

    feat(p8): part-8-rtk-query-advanced#implementing-optimistic-updates

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -32,6 +32,36 @@ export const apiSlice = createApi({
       }),
       invalidatesTags: (result, error, arg) => [{ type: 'Post', id: arg.id }],
     }),
+    addReaction: builder.mutation({
+      query: ({ postId, reaction }) => ({
+        url: `posts/${postId}/reactions`,
+        method: 'POST',
+        // In a real app, we'd probably need to base this on user ID somehow
+        // so that a user can't do the same reaction more than once
+        body: { reaction },
+      }),
+      async onQueryStarted({ postId, reaction }, { dispatch, queryFulfilled }) {
+        // `updateQueryData` requires the endpoint name and cache key arguments,
+        // so it knows which piece of cache state to update
+        const patchResult = dispatch(
+          apiSlice.util.updateQueryData('getPosts', undefined, (draft) => {
+            // The `draft` is Immer-wrapped and can be "mutated" like in createSlice
+            const post = draft.find((post) => post.id === postId)
+            if (post) {
+              post.reactions[reaction]++
+            }
+          })
+        )
+        try {
+          await queryFulfilled
+        } catch {
+          patchResult.undo()
+        }
+      },
+      // invalidatesTags: (result, error, arg) => [
+      //   { type: 'Post', id: arg.postId },
+      // ],
+    }),
   }),
 })
 export const {
@@ -40,4 +70,5 @@ export const {
   useGetUsersQuery,
   useAddNewPostMutation,
   useEditPostMutation,
+  useAddReactionMutation,
 } = apiSlice
--- src/features/users/UserPage.js
+++ src/features/users/UserPage.js
@@ -1,20 +1,44 @@
-import React from 'react'
+import { createSelector } from '@reduxjs/toolkit'
+import React, { useMemo } from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 import { selectUserById } from '../users/usersSlice'
-import { selectAllPosts } from '../posts/postsSlice'
+import { useGetPostsQuery } from '../api/apiSlice'
+// import { selectAllPosts } from '../posts/postsSlice'
 
 export const UserPage = ({ match }) => {
   const { userId } = match.params
 
   const user = useSelector((state) => selectUserById(state, userId))
 
-  const postsForUser = useSelector((state) => {
-    const allPosts = selectAllPosts(state)
-    return allPosts.filter((post) => post.user === userId)
-  })
+  // const postsForUser = useSelector((state) => {
+  //   const allPosts = selectAllPosts(state)
+  //   return allPosts.filter((post) => post.user === userId)
+  // })
 
+  const selectPostsForUser = useMemo(() => {
+    const emptyArray = []
+    // Return a unique selector instance for this page so that
+    // the filtered results are correctly memoized
+    return createSelector(
+      (res) => res.data,
+      (res, userId) => userId,
+      (data, userId) =>
+        data?.filter((post) => post.user === userId) ?? emptyArray
+    )
+  }, [])
+
+  // Use the same posts query, but extract only part of its data
+  const { postsForUser } = useGetPostsQuery(undefined, {
+    selectFromResult: (result) => ({
+      // We can optionally include the other metadata fields from the result here
+      ...result,
+      // Include a field called `postsForUser` in the hook result object,
+      // which will be a filtered list of posts
+      postsForUser: selectPostsForUser(result, userId),
+    }),
+  })
   const postTitles = postsForUser.map((post) => (
     <li key={post.id}>
       <Link to={`/posts/${post.id}`}>{post.title}</Link>
@@ -24,7 +48,6 @@ export const UserPage = ({ match }) => {
   return (
     <section>
       <h2>{user.name}</h2>
-
       <ul>{postTitles}</ul>
     </section>
   )
